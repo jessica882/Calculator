0d07e9d6f97871e4a1a8aa6d456567a1dbafbc7b
Jessica Shrestha <jessicastha019@gmail.com>
2025-01-09 21:41:37 +0545

    Postfix conversion

--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf9847ff53076dbff1b38082771a046b05dda59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ec0f9a1deac703dbbcbf66c38ce06cee4daa62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
